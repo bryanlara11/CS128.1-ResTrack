013b43a8fc4bf362a9640342d56c233a6f7221b3
mariaamor <mgeco@up.edu.ph>
2026-04-15 20:49:47 +0800

    Removed stat box

--- restrack-frontend/src/components/pages/DashboardResearcher.js
+++ restrack-frontend/src/components/pages/DashboardResearcher.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./DashboardResearcher.module.css";
 
-import StatBox from "../dashboard/StatBox";
 import Notifications from "../dashboard/Notifications";
 import Status from "../dashboard/Status";
 import RecentStudies from "../dashboard/RecentStudies";
@@ -9,17 +8,21 @@ import RecentStudies from "../dashboard/RecentStudies";
 function DashboardResearcher() {
   return (
     <div className={styles.dashboard}>
-      <div className={styles.columns}>
+
+      <div className={styles.statusRow}>
+        <Status />
+      </div>
+
+      <div className={styles.bottomRow}>
         <div className={styles.leftColumn}>
-          <StatBox />
           <Notifications />
         </div>
 
         <div className={styles.rightColumn}>
-          <Status />
           <RecentStudies />
         </div>
       </div>
+
     </div>
   );
 }
